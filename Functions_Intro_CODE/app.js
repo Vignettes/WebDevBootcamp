fede8e47703b94fdaaf9edeb7a60d5e884b3516a
George Wolf <gwolf204@gmail.com>
2021-02-08 16:41:59 -0500

    test

--- Functions_Intro_CODE/app.js
+++ Functions_Intro_CODE/app.js
@@ -24,18 +24,36 @@ function add(x, y) {
 }
 
 
+/* Below function is to create the same message 3 times in uppercase */
+
+/* function rant(message) {
+    for(let i = 0; i < 3; i++) {
+        console.log(message.toUpperCase());
+    }
+}
+ */
+/* rant('beans'); */
+
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
 
 
+// define isSnakeEyes below:
+/* 
+function isSnakeEyes(numOne, numTwo) {
+    if (numOne === 1 && numTwo === 1) {
+        console.log("Snake Eyes!") }
+        else (console.log("Not Snake Eyes!")) 
+    }
 
-// singSong()
-// singSong()
-// singSong()
-// singSong()
-// singSong()
-// singSong()
-// singSong()
-// singSong()
-// singSong()
-// singSong()
-// singSong()
+isSnakeEyes(1,1);
+isSnakeEyes(2,1); */
+
+/* Return statement will end a functions execution AND specifies the value to be returned by that function */
 
